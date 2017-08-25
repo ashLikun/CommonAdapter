ffc99f91029d8ab3bdcda0a5b3195b8df115ec17
likun <likun01@vcredit.com>
2017-08-25 17:51:52 +0800

    DatBindAdapter

--- adapter/src/main/java/com/ashlikun/adapter/recyclerview/BaseAdapter.java
+++ adapter/src/main/java/com/ashlikun/adapter/recyclerview/BaseAdapter.java
@@ -1,11 +1,15 @@
 package com.ashlikun.adapter.recyclerview;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 
 import com.ashlikun.adapter.ViewHolder;
+import com.ashlikun.adapter.animation.BaseAnimation;
 import com.ashlikun.adapter.recyclerview.click.OnItemClickListener;
 import com.ashlikun.adapter.recyclerview.click.OnItemLongClickListener;
 
@@ -27,6 +31,12 @@ public abstract class BaseAdapter<T, V extends RecyclerView.ViewHolder> extends 
     private int footerSize;
     OnItemClickListener onItemClickListener;
     OnItemLongClickListener onItemLongClickListener;
+    private boolean mOpenAnimationEnable = false;//动画是否开启
+    private boolean mFirstOnlyEnable = true;//仅仅第一次底部出现才动画
+    private int mLastPosition = -1;//最后一个显示的item
+    private BaseAnimation mCustomAnimation;//动画
+    private Interpolator mInterpolator = new LinearInterpolator();
+    private int mDuration = 300;
 
     public BaseAdapter(Context context, int layoutId, List<T> datas) {
         mContext = context;
@@ -129,4 +139,39 @@ public abstract class BaseAdapter<T, V extends RecyclerView.ViewHolder> extends 
         this.headerSize = headerSize;
     }
 
+    @Override
+    public void onViewAttachedToWindow(V holder) {
+        super.onViewAttachedToWindow(holder);
+        int type = holder.getItemViewType();
+        addAnimation(holder);
+    }
+
+    /**
+     * add animation when you want to show time
+     *
+     * @param holder
+     */
+    private void addAnimation(RecyclerView.ViewHolder holder) {
+        if (mOpenAnimationEnable) {
+            if (!mFirstOnlyEnable || holder.getLayoutPosition() > mLastPosition) {
+                if (mCustomAnimation != null) {
+                    for (Animator anim : mCustomAnimation.getAnimators(holder.itemView)) {
+                        startAnim(anim, holder.getLayoutPosition());
+                    }
+                }
+                mLastPosition = holder.getLayoutPosition();
+            }
+        }
+    }
+
+    /**
+     * set anim to start when loading
+     *
+     * @param anim
+     * @param index
+     */
+    protected void startAnim(Animator anim, int index) {
+        anim.setDuration(mDuration).start();
+        anim.setInterpolator(mInterpolator);
+    }
 }
