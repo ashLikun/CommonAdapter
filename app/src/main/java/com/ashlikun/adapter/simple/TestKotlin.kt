4a44b2a66ff657af10bb8a36592a8e3183a72378
李坤 <496546144@qq.com>
2021-12-09 22:11:28 +0800

    优化

--- app/src/main/java/com/ashlikun/adapter/simple/TestKotlin.kt
+++ app/src/main/java/com/ashlikun/adapter/simple/TestKotlin.kt
@@ -13,11 +13,13 @@ fun main(args: Array<String>) {
 }
 
 open class ACls(
-        open var a: Int = 1,
-        open var b: String = "ad",
-        open var c: Int = 2
+    open var a: Int = 1,
+    open var b: String = "ad",
+    open var c: Int = 2
 ) {
     val ss = a + c
+
+    open var maxHeight = 1f
 
     init {
         print()
@@ -33,19 +35,38 @@ open class ACls(
 }
 
 class BCls(
-        override var a: Int = 10,
-        override var b: String = "StringStringString",
-        override var c: Int = 100
+    override var a: Int = 10,
+    override var b: String = "StringStringString",
+    override var c: Int = 100
 ) : ACls(a = a, b = b, c = c) {
+
+    var maxRatio: Float = 0f
+        set(value) {
+            field = value
+        }
+
     init {
         print()
         println(11111111111111)
     }
 
-    override fun print(): Unit {
+    override fun print() {
+        maxRatio = 20f
         println("aa=${a}")
         println("bb=${b}")
         println("cc=${c}")
         println("sss=${ss}")
+        println("maxRatio=${maxRatio}")
+        if (this is ACls) {
+            println("dddddddddddddd")
+        }
+        var position = 11
+        if (position in -1..10) {
+            println("ssssssssssss")
+        }
+        val aaad = null
+        if (aaad is ACls) {
+            println("aaad")
+        }
     }
 }
