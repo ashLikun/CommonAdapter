b5ec31ad6aa68befef295dd3c4d24c90b5b5d06c
李坤 <496546144@qq.com>
2021-12-22 14:47:41 +0800

    优化全部切换为Kotlin,并且简化写法

--- adapter/src/main/java/com/ashlikun/adapter/ViewHolder.kt
+++ adapter/src/main/java/com/ashlikun/adapter/ViewHolder.kt
@@ -33,9 +33,9 @@ import com.ashlikun.adapter.recyclerview.vlayout.IStartPosition
  */
 
 open class ViewHolder(
-        var mContext: Context,
-        var createView: CreateView,
-        var iStartPosition: IStartPosition
+    var mContext: Context,
+    var createView: CreateView,
+    var iStartPosition: IStartPosition
 ) : RecyclerView.ViewHolder(createView.view) {
     protected var mViews = SparseArray<View>()
 
@@ -88,18 +88,19 @@ open class ViewHolder(
      * @param viewId
      * @return
      */
-    fun <T : View?> getView(viewId: Int): T? {
+    fun <T : View> getView(viewId: Int): T {
         var view = mViews[viewId]
         if (view == null) {
             view = itemView.findViewById(viewId)
             mViews.put(viewId, view)
         }
-        return view as T?
+        if (view == null) throw RuntimeException("没有发现 viewId")
+        return view as T
     }
 
 
     fun getImageView(viewId: Int): ImageView {
-        return getView<ImageView>(viewId)!!
+        return getView<ImageView>(viewId)
     }
 
     /**
@@ -110,93 +111,93 @@ open class ViewHolder(
      * @return
      */
     fun setText(viewId: Int, text: CharSequence?): ViewHolder {
-        val tv = getView<TextView>(viewId)!!
+        val tv = getView<TextView>(viewId)
         tv.text = text
         return this
     }
 
     fun setText(viewId: Int, text: Number): ViewHolder {
-        val tv = getView<TextView>(viewId)!!
+        val tv = getView<TextView>(viewId)
         tv.text = text.toString()
         return this
     }
 
     fun setImageResource(viewId: Int, resId: Int): ViewHolder {
-        val view = getView<ImageView>(viewId)!!
+        val view = getView<ImageView>(viewId)
         view.setImageResource(resId)
         return this
     }
 
     fun setImageBitmap(viewId: Int, bitmap: Bitmap?): ViewHolder {
-        val view = getView<ImageView>(viewId)!!
+        val view = getView<ImageView>(viewId)
         view.setImageBitmap(bitmap)
         return this
     }
 
     fun setImageDrawable(viewId: Int, drawable: Drawable?): ViewHolder {
-        val view = getView<ImageView>(viewId)!!
+        val view = getView<ImageView>(viewId)
         view.setImageDrawable(drawable)
         return this
     }
 
     fun setBackgroundColor(viewId: Int, color: Int): ViewHolder {
-        val view = getView<View>(viewId)!!
+        val view = getView<View>(viewId)
         view.setBackgroundColor(color)
         return this
     }
 
     fun setBackgroundRes(viewId: Int, backgroundRes: Int): ViewHolder {
-        val view = getView<View>(viewId)!!
+        val view = getView<View>(viewId)
         view.setBackgroundResource(backgroundRes)
         return this
     }
 
     fun setBackgroundDrawable(viewId: Int, drawable: Drawable?): ViewHolder {
-        val view = getView<View>(viewId)!!
+        val view = getView<View>(viewId)
         ViewCompat.setBackground(view, drawable)
         return this
     }
 
     fun setTextColor(viewId: Int, textColor: Int): ViewHolder {
-        val view = getView<TextView>(viewId)!!
+        val view = getView<TextView>(viewId)
         view.setTextColor(textColor)
         return this
     }
 
     fun setTextColorRes(viewId: Int, textColorRes: Int): ViewHolder {
-        val view = getView<TextView>(viewId)!!
+        val view = getView<TextView>(viewId)
         view.setTextColor(mContext.resources.getColor(textColorRes))
         return this
     }
 
     fun setAlpha(viewId: Int, value: Float): ViewHolder {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            getView<View>(viewId)!!.alpha = value
+            getView<View>(viewId).alpha = value
         } else {
             // Pre-honeycomb hack to set Alpha value
             val alpha = AlphaAnimation(value, value)
             alpha.duration = 0
             alpha.fillAfter = true
-            getView<View>(viewId)!!.startAnimation(alpha)
+            getView<View>(viewId).startAnimation(alpha)
         }
         return this
     }
 
     fun setVisible(viewId: Int, visible: Boolean): ViewHolder {
-        val view = getView<View>(viewId)!!
+        val view = getView<View>(viewId)
         view.visibility = if (visible) View.VISIBLE else View.GONE
         return this
     }
 
     fun linkify(viewId: Int): ViewHolder {
-        val view = getView<TextView>(viewId)!!
+        val view = getView<TextView>(viewId)
         Linkify.addLinks(view, Linkify.ALL)
         return this
     }
 
     fun setTypeface(typeface: Typeface?, vararg viewIds: Int): ViewHolder {
         for (viewId in viewIds) {
-            val view = getView<TextView>(viewId)!!
+            val view = getView<TextView>(viewId)
             view.typeface = typeface
             view.paintFlags = view.paintFlags or Paint.SUBPIXEL_TEXT_FLAG
         }
@@ -204,45 +205,45 @@ open class ViewHolder(
     }
 
     fun setProgress(viewId: Int, progress: Int): ViewHolder {
-        val view = getView<ProgressBar>(viewId)!!
+        val view = getView<ProgressBar>(viewId)
         view.progress = progress
         return this
     }
 
     fun setProgress(viewId: Int, progress: Int, max: Int): ViewHolder {
-        val view = getView<ProgressBar>(viewId)!!
+        val view = getView<ProgressBar>(viewId)
         view.max = max
         view.progress = progress
         return this
     }
 
     fun setMax(viewId: Int, max: Int): ViewHolder {
-        val view = getView<ProgressBar>(viewId)!!
+        val view = getView<ProgressBar>(viewId)
         view.max = max
         return this
     }
 
     fun setRating(viewId: Int, rating: Float): ViewHolder {
-        val view = getView<RatingBar>(viewId)!!
+        val view = getView<RatingBar>(viewId)
         view.rating = rating
         return this
     }
 
     fun setRating(viewId: Int, rating: Float, max: Int): ViewHolder {
-        val view = getView<RatingBar>(viewId)!!
+        val view = getView<RatingBar>(viewId)
         view.max = max
         view.rating = rating
         return this
     }
 
     fun setTag(viewId: Int, tag: Any?): ViewHolder {
-        val view = getView<View>(viewId)!!
+        val view = getView<View>(viewId)
         view.tag = tag
         return this
     }
 
     fun setTag(viewId: Int, key: Int, tag: Any?): ViewHolder {
-        val view = getView<View>(viewId)!!
+        val view = getView<View>(viewId)
         view.setTag(key, tag)
         return this
     }
@@ -257,8 +258,8 @@ open class ViewHolder(
         var params = view.layoutParams
         if (params == null) {
             params = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
             )
         }
         params.height = size
@@ -272,28 +273,28 @@ open class ViewHolder(
      * 关于事件的
      */
     fun setOnClickListener(
-            viewId: Int,
-            listener: View.OnClickListener?
+        viewId: Int,
+        listener: View.OnClickListener?
     ): ViewHolder {
-        val view = getView<View>(viewId)!!
+        val view = getView<View>(viewId)
         view.setOnClickListener(listener)
         return this
     }
 
     fun setOnTouchListener(
-            viewId: Int,
-            listener: OnTouchListener?
+        viewId: Int,
+        listener: OnTouchListener?
     ): ViewHolder {
-        val view = getView<View>(viewId)!!
+        val view = getView<View>(viewId)
         view.setOnTouchListener(listener)
         return this
     }
 
     fun setOnLongClickListener(
-            viewId: Int,
-            listener: OnLongClickListener?
+        viewId: Int,
+        listener: OnLongClickListener?
     ): ViewHolder {
-        val view = getView<View>(viewId)!!
+        val view = getView<View>(viewId)
         view.setOnLongClickListener(listener)
         return this
     }
