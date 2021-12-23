4102a76b55855125095238c04b856604b699db24
爱生活、李坤 <496546144@qq.com>
2021-12-23 23:02:49 +0800

    优化

--- adapter/src/main/java/com/ashlikun/adapter/recyclerview/vlayout/CommonMultipleAdapter.kt
+++ adapter/src/main/java/com/ashlikun/adapter/recyclerview/vlayout/CommonMultipleAdapter.kt
@@ -11,7 +11,7 @@ import com.ashlikun.adapter.recyclerview.common.CommonAdapter
  * 功能介绍：[MultipleAdapterHelp]的简化版
  */
 open class CommonMultipleAdapter(recyclerView: RecyclerView) : MultipleAdapterHelp(recyclerView) {
-    fun bindUi(vararg adapters: CommonAdapter<Any>): CommonMultipleAdapter {
+    fun bindUi(vararg adapters: CommonAdapter<*>): CommonMultipleAdapter {
         adapter.addAdapters(adapters.toMutableList())
         return this
     }
